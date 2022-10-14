ab25b82cb0a23407a8d03c1f8378ada9cabe118f
agungtrue <syuhadadwiagung@gmail.com>
2022-10-14 20:55:30 +0700

    integration some api and styling

--- src/pages/alerts/index.jsx
+++ src/pages/alerts/index.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import {
-    Link,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import './alerts.scss';
+import { userLogin } from '../../services/userToken';
+import axios from 'axios';
 
 import Header from '../../components/Header';
 import SideBarList from '../../components/SideBarList';
@@ -10,7 +9,43 @@ import Content from '../../components/Content';
 
 function Alerts() {
     const [selectedData, setSelectedData] = useState({});
-    
+    const [isRecallCounter, setIsRecallCounter] = useState(0);
+    const [suspectedReasonState, setSuspectedReasonState] = useState([]);
+
+    // this function to make some listeners, so can able to tell child to refetch the api list
+    function reCalled() {
+        setIsRecallCounter((prev) => prev+1)
+    };
+
+    async function getSuspectedReason () {
+        const API_URL = process.env.REACT_APP_API_URL;
+        const { token } = userLogin();
+
+        // init, using static token, since token are required
+        try {
+            const { data } = await axios.get(`${API_URL}/api/suspected_reason`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            // get response
+            const response = data?.data;
+            if (!response) throw new Error(); // check if fail
+
+            // set alert state
+            setSuspectedReasonState(response)
+
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        getSuspectedReason();
+    }, [])
+
     return (
         <>
             <Header />
@@ -26,8 +61,13 @@ function Alerts() {
                     <main className="alerts__main__content">
                         <SideBarList
                             callbackSelected={setSelectedData}
+                            isReFetch={isRecallCounter}
                         />
-                        <Content data={selectedData} />
+                        <Content
+                            data={selectedData}
+                            callbackList={reCalled}
+                            suspectedReason={suspectedReasonState}
+                        />
                     </main>
                 </div>
             </div>
